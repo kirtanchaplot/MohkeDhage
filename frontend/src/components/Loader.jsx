58fd5dd1f6789af3dbd2613fd7b705023bc32636
kirtanchaplot <kirtanchaplot@gmail.com>
2025-03-21 19:17:50 +0530

    Done

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -9,10 +9,3 @@ const Loader = () => {
 export default Loader;
 
 
-// const Loader = () => {
-//     return (
-//       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-pink-500 border-opacity-50"></div>
-//     );
-//   };
-  
-//   export default Loader;
