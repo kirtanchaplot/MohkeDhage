2892017a2c617e756dab7231b2465785dfb0aa7f
kirtanchaplot <kirtanchaplot@gmail.com>
2025-03-22 19:55:36 +0530

    Done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      // Remove localhost since backend is on Render
     },
   },
-})
+});
+
+
+
+
+
+
+
+//   server: {
+//     proxy: {
+//       "/api/": "http://localhost:5000",
+//       "/uploads/": "http://localhost:5000",
+//     },
+//   },
+// })
