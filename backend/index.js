a21e27d7a1a7f2e543282bc2ce85bbdca8a7060d
kirtanchaplot <kirtanchaplot@gmail.com>
2025-03-23 11:25:56 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-const app = express();
+// const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
