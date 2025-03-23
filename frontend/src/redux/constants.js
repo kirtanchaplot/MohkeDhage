24435601779bf14c15f662e5cfe8100c07e32866
kirtanchaplot <kirtanchaplot@gmail.com>
2025-03-23 11:41:53 +0530

    Done

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,18 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const RAZORPAY_URL = "/api/config/razorpay";
+// export const BASE_URL = "";
+// export const USERS_URL = "/api/users";
+// export const CATEGORY_URL = "/api/category";
+// export const PRODUCT_URL = "/api/products";
+// export const UPLOAD_URL = "/api/upload";
+// export const ORDERS_URL = "/api/orders";
+// export const RAZORPAY_URL = "/api/config/razorpay";
+
+
+
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"; 
+
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const PRODUCT_URL = `${BASE_URL}/api/products`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const RAZORPAY_URL = `${BASE_URL}/api/config/razorpay`;
