da4fc43ddd36c5d39af8f7ae1779122e691f4653
kirtanchaplot <kirtanchaplot@gmail.com>
2025-03-23 21:26:16 +0530

    D

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    // content: ["./index.html", 
-    //         "./src/**/*.{js,ts,jsx,tsx}"],
-    content: [
-        "./frontend/index.html",  // Update to reflect the correct path
-        "./frontend/src/**/*.{js,ts,jsx,tsx}", // Adjusted for 'src' inside 'frontend'
-      ],
+    content: ["./index.html", 
+            "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
       extend: {},
     },
