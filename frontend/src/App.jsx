51dd1bfe5c2791789372978f89ca5242b31301d4
kirtanchaplot <kirtanchaplot@gmail.com>
2025-03-23 22:58:38 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import './index.css'; 
+
 
 const App = () => {
   return (
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // "./frontend/src/**/*.{js,ts,jsx,tsx}"
+    //  "./index.html", 
+    // "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
+
+
+
+
+
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: ["./index.html", 
+//             "./src/**/*.{js,ts,jsx,tsx}"],
+//             safelist: [
+//                 "bg-red-500",
+//                 "text-white",
+//                 "p-4",
+//                 "rounded-md",
+//               ], 
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   };
+  
+
+// module.exports = {
+//     content: [
+//       "./index.html",                 //same for above
+//       "./src/**/*.{js,ts,jsx,tsx}",
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   };
+  
